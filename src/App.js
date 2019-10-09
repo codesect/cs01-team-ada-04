5c3de1cf57ec41a2e58ed2e362dab973f255d0e3
Zsolt Meszaros <zsolt@sowad.com>
2019-10-09 22:31:37 +0100

    Display a random string on page load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import generatePassword from './utils/generatePassword';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Password generator</h1>
+        <p>
+          Here is a random password: <code>{generatePassword()}</code>
+        </p>
       </header>
     </div>
   );
