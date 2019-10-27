2a341d7645fc6816fd80ade9e274f1188ec6b04f
Zsolt Meszaros <zsolt@sowad.com>
2019-10-27 10:42:34 +0000

    Add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (e) {
+      console.log(e);
+      return initialValue;
+    }
+  });
+
+  function setValue(value) {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return [storedValue, setValue];
+}
