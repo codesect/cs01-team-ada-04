ab2fd79d2c9bd292e6eb78e6a4ae1dc2e1e609dd
Mizou9999 <mizou9999@yandex.ru>
2019-11-09 19:50:07 +0300

    add some styles to the slider

--- src/components/Range.js
+++ src/components/Range.js
@@ -1,18 +1,81 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  label {
+    margin-right: 10px;
+  }
+  output {
+    background: white;
+    margin: 10px;
+    padding: 6px 10px;
+    border-radius: 6px;
+    border: 1px solid ${({ theme }) => theme.border};
+  }
+`;
 const Input = styled.input`
+  align-items: center;
   -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
   width: 100%;
-  color: red;
+  &::-webkit-progress-bar {
+    background-color: red;
+  }
+  &::-webkit-slider-thumb {
+    height: 22px;
+    width: 22px;
+    border-radius: 50%;
+    align-self: center;
+    background: ${({ theme }) => theme.button};
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -10px;
+  }
+  &::-moz-range-thumb {
+    height: 22px;
+    width: 22px;
+    border-radius: 50%;
+    align-self: center;
+    background: ${({ theme }) => theme.button};
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -10px;
+    border: none;
+  }
+  &::-webkit-slider-runnable-track {
+    background: ${({ theme }) => theme.borderToggle};
+    height: 2px;
+  }
+
+  &::-moz-range-track {
+    background: ${({ theme }) => theme.borderToggle};
+  }
+  &::-moz-range-progress {
+    background-color: ${({ theme }) => theme.footerBackground};
+  }
+  &:focus {
+    outline: none;
+  }
+  &:-moz-focus-outer {
+    border: 0;
+  }
 `;
 function Range({ id, min, max, value, step, onChange }) {
   return (
-    <>
+    <Container>
       <output htmlFor={id} id={`${id}-output`}>
         {value}
       </output>
-      <label htmlFor="id">Length</label>
       <Input
         type="range"
         id={id}
@@ -25,7 +88,7 @@ function Range({ id, min, max, value, step, onChange }) {
         step={step}
         value={value}
       />
-    </>
+    </Container>
   );
 }
 
