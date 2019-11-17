abb59147c33af4b3b902c2e601f6cdeee897a87a
Mizou9999 <mizou9999@yandex.ru>
2019-11-17 12:31:24 +0300

    add propTypes

--- src/components/Range.js
+++ src/components/Range.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import PropTypes from 'prop-types';
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 
-  label {
-    margin-right: 10px;
-  }
   output {
-    background: white;
+    background-color: ${({ theme }) => theme.input};
     margin: 10px;
     padding: 6px 10px;
-    border-radius: 6px;
+    border-radius: ${({ theme }) => theme.borderRadius};
     border: 1px solid ${({ theme }) => theme.border};
   }
 `;
 const Input = styled.input`
   align-items: center;
-  -webkit-appearance: none;
-  -moz-appearance: none;
   appearance: none;
   background: none;
-  border: none;
+  border: 0;
   cursor: pointer;
-  display: flex;
-  align-items: center;
   width: 100%;
-  &::-webkit-progress-bar {
-    background-color: red;
-  }
+
   &::-webkit-slider-thumb {
     height: 22px;
     width: 22px;
     border-radius: 50%;
     align-self: center;
-    background: ${({ theme }) => theme.button};
+    background-color: ${({ theme }) => theme.button};
     cursor: pointer;
     -webkit-appearance: none;
     margin-top: -10px;
@@ -46,25 +38,25 @@ const Input = styled.input`
     width: 22px;
     border-radius: 50%;
     align-self: center;
-    background: ${({ theme }) => theme.button};
+    background-color: ${({ theme }) => theme.button};
     cursor: pointer;
     -webkit-appearance: none;
     margin-top: -10px;
-    border: none;
+    border: 0;
   }
   &::-webkit-slider-runnable-track {
-    background: ${({ theme }) => theme.borderToggle};
+    background-color: ${({ theme }) => theme.borderToggle};
     height: 2px;
   }
 
   &::-moz-range-track {
-    background: ${({ theme }) => theme.borderToggle};
+    background-color: ${({ theme }) => theme.borderToggle};
   }
   &::-moz-range-progress {
     background-color: ${({ theme }) => theme.footerBackground};
   }
   &:focus {
-    outline: none;
+    outline-style: none;
   }
   &:-moz-focus-outer {
     border: 0;
@@ -73,10 +65,9 @@ const Input = styled.input`
 function Range({ id, min, max, value, step, onChange }) {
   return (
     <Container>
-      <output htmlFor={id} id={`${id}-output`}>
-        {value}
-      </output>
+      <output htmlFor={id}>{value}</output>
       <Input
+        aria-label="Range Slider"
         type="range"
         id={id}
         aria-valuemin={min}
@@ -85,11 +76,20 @@ function Range({ id, min, max, value, step, onChange }) {
         min={min}
         max={max}
         onChange={onChange}
-        step={step}
         value={value}
       />
     </Container>
   );
 }
+Range.defaultProps = {
+  step: 1,
+};
+Range.propTypes = {
+  id: PropTypes.string,
+  min: PropTypes.number,
+  max: PropTypes.number,
+  onChange: PropTypes.func,
+  value: PropTypes.number,
+};
 
 export default Range;
